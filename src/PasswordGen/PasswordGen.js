9cafb528fdd4edebbda063c7c2b650a367cb2249
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-12 20:40:58 +0000

    aligned box center

--- src/PasswordGen/PasswordGen.js
+++ src/PasswordGen/PasswordGen.js
@@ -8,17 +8,19 @@ const PasswordGen = () => {
     setPassword(Math.round(Math.random() * max));
   };
   return (
-    <div className='border-2 border-white w-24'>
-      <h1 className='font-bold border border-red-600 text-white'>
-        Password Generator
-      </h1>
-      <p className='border-2 border-white text-white'>{password}</p>
-      <button
-        onClick={getRandomPassword}
-        className='rounded-lg border-2 border-white text-white'
-      >
-        Generate Password
-      </button>
+    <div className='flex flex-col items-center justify-center'>
+      <div className='border-2 border-white w-24 rounded-md '>
+        <h1 className='font-bold border border-red-600 text-white'>
+          Password Generator
+        </h1>
+        <p className='border-2 border-white text-white'>{password}</p>
+        <button
+          onClick={getRandomPassword}
+          className='rounded-lg border-2 border-white text-white'
+        >
+          Generate Password
+        </button>
+      </div>
     </div>
   );
 };
