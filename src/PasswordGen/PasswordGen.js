b766e841031d02ea6199498943f49b23a420274a
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-12 20:15:09 +0000

    added some jsx and changed background color

--- src/PasswordGen/PasswordGen.js
+++ src/PasswordGen/PasswordGen.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PasswordGen = () => {
-  return <div></div>;
+  const [password, setPassword] = useState('');
+  return (
+    <div className='border-2 border-white w-24'>
+      <h1 className='font-bold border border-red-600 text-white'>
+        Password Generator
+      </h1>
+      <p className='border-2 border-white text-white'>
+        Placeholder for password
+      </p>
+      <button className='rounded-lg border-2 border-white text-white'>
+        Generate Password
+      </button>
+    </div>
+  );
 };
 
 export default PasswordGen;
