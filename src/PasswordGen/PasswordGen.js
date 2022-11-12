8832c2fc3097ccd835f9e9a9c59c15eebcff42cc
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-12 20:33:08 +0000

    added random logic not completed yet

--- src/PasswordGen/PasswordGen.js
+++ src/PasswordGen/PasswordGen.js
@@ -3,15 +3,20 @@ import React, { useState } from 'react';
 const PasswordGen = () => {
   document.title = 'Password Generator';
   const [password, setPassword] = useState('');
+  const getRandomPassword = () => {
+    let max = '99999999';
+    setPassword(Math.round(Math.random() * max));
+  };
   return (
     <div className='border-2 border-white w-24'>
       <h1 className='font-bold border border-red-600 text-white'>
         Password Generator
       </h1>
-      <p className='border-2 border-white text-white'>
-        Placeholder for password
-      </p>
-      <button className='rounded-lg border-2 border-white text-white'>
+      <p className='border-2 border-white text-white'>{password}</p>
+      <button
+        onClick={getRandomPassword}
+        className='rounded-lg border-2 border-white text-white'
+      >
         Generate Password
       </button>
     </div>
