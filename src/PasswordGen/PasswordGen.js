29b8a7f5452db371980fe46f98b87bba22b013c4
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-13 02:53:14 +0000

    added copy password button

--- src/PasswordGen/PasswordGen.js
+++ src/PasswordGen/PasswordGen.js
@@ -5,12 +5,11 @@ const PasswordGen = () => {
   const [password, setPassword] = useState('');
 
   const getRandomPassword = () => {
-    // let test = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let max = `99999999`;
     setPassword(Math.floor(Math.random() * max));
   };
   return (
-    <div className='flex flex-col items-center justify-center my-auto  '>
+    <div className='flex flex-col items-center justify-center my-auto'>
       <div className=' border-2 border-white w-1/2 rounded-md flex flex-col items-center pb-24'>
         <h1 className='font-bold text-3xl text-white mt-10'>
           Password Generator
@@ -18,11 +17,14 @@ const PasswordGen = () => {
         <p className='border-2 border-white text-white p-3 rounded-2xl mt-10 font-bold '>
           {password}
         </p>
-        <button
-          onClick={getRandomPassword}
-          className='rounded-lg border-2 border-white text-white mt-14 p-2 font-bold'
-        >
+        <button onClick={getRandomPassword} className='btn'>
           Generate Password
+        </button>
+        <button
+          className='btn'
+          onClick={() => navigator.clipboard.writeText(getRandomPassword)}
+        >
+          Copy Password
         </button>
       </div>
     </div>
